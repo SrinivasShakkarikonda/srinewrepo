47d96697f4a3143fbf79f10bec4669a7fa57e4d7
Sri1 <sri4nivas5@gmail.com>
2019-05-08 17:11:09 -0400

    Hello, This is first eclipse push.

---
+++ repo file/src/helloworldprog.java
@@ -0,0 +1,16 @@
+
+public class helloworldprog {
+	int a=55;
+	int b=66;
+	void add()
+	{
+		int c=a+b;
+		System.out.println("print the value of addition="+c);
+	}
+	public static void main(String args[])
+	{
+	helloworldprog obj=new helloworldprog();
+			obj.add();
+
+}
+}
